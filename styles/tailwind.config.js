22fafefae3d75d3d1576dbd2c679732df8f658d6
Grant Holle <hollegrant@gmail.com>
2021-07-24 13:23:51 +0800

    House cleaning

--- styles/tailwind.config.js
+++ styles/tailwind.config.js
@@ -1,4 +1,5 @@
-const autoprefixer = require('autoprefixer');
+const colors = require('tailwindcss/colors')
+const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   important: true,
@@ -19,6 +20,39 @@ module.exports = {
       center: true,
     },
     extend: {
+      colors: {
+        gray: colors.coolGray,
+        primary: {
+          '50': '#edfafa',
+          '100': '#d5f5f6',
+          '200': '#afecef',
+          '300': '#7edce2',
+          '400': '#16bdca',
+          '500': '#0694a2',
+          '600': '#047481',
+          '700': '#036672',
+          '800': '#05505c',
+          '900': '#014451',
+        },
+        blue: {
+          50: '#eff6ff',
+          100: '#dbeafe',
+          200: '#bfdbfe',
+          300: '#93c5fd',
+          400: '#60a5fa',
+          500: '#3b82f6',
+          600: '#2563eb',
+          700: '#1d4ed8',
+          800: '#1e40af',
+          900: '#1e3a8a',
+        },
+      },
+      fontFamily: {
+        sans: [
+          `'Inter'`,
+          ...defaultTheme.fontFamily.sans,
+        ]
+      },
       typography: {
         DEFAULT: {
           css: {
@@ -53,7 +87,7 @@ module.exports = {
         },
       }
     },
-  }, 
+  },
   variants: {},
   plugins: [
     require('@tailwindcss/typography'),
